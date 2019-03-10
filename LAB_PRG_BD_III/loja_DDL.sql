2c00f66829c10413b6a35a33c73c4d20bb5f6d90
Leonardo Rodrigues Miranda Lins <44787835+leonardormlins@users.noreply.github.com>
2019-03-09 21:46:37 -0300

    Update loja_DDL.sql

--- LAB_PRG_BD_III/loja_DDL.sql
+++ LAB_PRG_BD_III/loja_DDL.sql
@@ -1,1 +1,32 @@
+---------------------------------------------------
+----Author: Leonardo Rodrigues Miranda Lins--------
+----Language: SQL----------------------------------
+----Tittle: DATABASE LOJA--------------------------
+----Status: Doing----------------------------------
+---------------------------------------------------
 
+CREATE TABLE CLI_CLIENTE(
+    CLI_COD NUMBER(5) PRIMARY KEY,
+    CLI_NOME VARCHAR2(100),
+    CLI_EMAIL VARCHAR2(50)
+);
+
+CREATE TABLE VEN_VENDEDOR(
+    VEN_COD NUMBER(4) PRIMARY KEY,
+    VEN_NOME VARCHAR2(100),
+    VEN_EMAIL VARCHAR2(50),
+);
+
+CREATE TABLE PRD_PRODUTO(
+    PRD_COD NUMBER(6) PRIMARY KEY,
+    PRD_VALOR_UNT NUMBER(6),
+    PRD_NOME VARCHAR2(30),
+    PRD_DESC VARCHAR2(150)
+);
+
+CREATE TABLE VND_VENDA(
+    VEN_COD NUMBER(4) REFERENCES VEN_VENDEDOR (VEN_COD),
+    CLI_COD NUMBER(5) REFERENCES CLI_CLIENTE (CLI_COD),
+    PRD_COD NUMBER(6) REFERENCES PRD_PRODUTO (PRD_COD),
+    VND_TOTAL NUMBER(12),
+);
