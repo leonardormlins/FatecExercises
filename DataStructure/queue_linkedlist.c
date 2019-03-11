9a38a81bc675ab683e1424b99021e72152705097
Leonardo Rodrigues Miranda Lins <44787835+leonardormlins@users.noreply.github.com>
2019-03-11 20:54:16 -0300

    Update queue_linkedlist.c

--- DataStructure/queue_linkedlist.c
+++ DataStructure/queue_linkedlist.c
@@ -1,1 +1,18 @@
+//---------------------------------------------------//
+//----Author: Leonardo Rodrigues Miranda Lins--------//
+//----Language: C------------------------------------//
+//----Tittle: Queue_Linked List----------------------//
+//----Status: Working--------------------------------//
+//---------------------------------------------------//
 
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+  int data;
+  struct Node *next;
+}*head=NULL;
+
+void add(Node**i, Node**t, int x){
+  
+}
