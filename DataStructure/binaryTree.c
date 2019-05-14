a5304c7dc5252adb0a5ccabf338f2ec076b78976
Leonardo Rodrigues Miranda Lins <44787835+leonardormlins@users.noreply.github.com>
2019-05-13 21:57:47 -0300

    Update binaryTree.c

--- DataStructure/binaryTree.c
+++ DataStructure/binaryTree.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+typedef struct no{
+  int valor;
+  struct no* direita;
+  struct no* esquerda;
+}No;
+
+int alturaArvore(No *raiz){
+	if(raiz==NULL){
+		return 0;
+	}
+	else{
+		he = 1 + alturaArvore(raiz->esquerda); 
+		hd = 1 + alturaArvore(raiz->direita);
+		if(he>hd) return he;
+		else return hd;
+	}
+}
+
+No* buscaPai(No *raiz, int alvo){
+	if(raiz->valor < alvo){
+		raiz->direita->valor == x ? return raiz: return buscaPai(raiz->direita, alvo);
+	}
+	else{
+		raiz->esquerda->valor == x ? return raiz : return buscaPai(raiz->esquerda, valor);
+	}
+}
+
+
+int main(){
+
+}
